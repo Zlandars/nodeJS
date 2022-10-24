0026e369187ab95871114f242ed72bdd5863bb33
Zlandars <mag20102009@gmail.com>
2022-10-24 12:40:49 +0300

    Lesson2 HW 1,2 ready

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import fs from 'fs';
-import { exists } from 'node:fs';
 import {Transform} from 'stream';
 
 const path = './storage/access.log';
-function randomizer(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min) + min);
-}
-const stringGenerator = () => {
-	const ip = `${randomizer(1,254)}.${randomizer(1,254)}.${randomizer(1,254)}.${randomizer(1,254)}`;
-	const date = new Date(randomizer(1,Date.now()));
-	const query = ['POST','GET'];
-	const serverAnswer = ['200','404','500'];
-	return `${ip} - - [${date} -0300] "${query[randomizer(0,query.length)]} /boo HTTP/1.1" ${serverAnswer[randomizer(0,serverAnswer.length)]} 0 "-" "curl/7.47.0"\n`;
-};
+
+// Генератор жирного лога Задание 1
+//
+// function randomizer(min, max) {
+// 	min = Math.ceil(min);
+// 	max = Math.floor(max);
+// 	return Math.floor(Math.random() * (max - min) + min);
+// }
+// Генератор строки лога
+// const stringGenerator = () => {
+// 	const ip = `${randomizer(1,254)}.${randomizer(1,254)}.${randomizer(1,254)}.${randomizer(1,254)}`;
+// 	const date = new Date(randomizer(1,Date.now()));
+// 	const query = ['POST','GET'];
+// 	const serverAnswer = ['200','404','500'];
+// 	return `${ip} - - [${date} -0300] "${query[randomizer(0,query.length)]} /boo HTTP/1.1" ${serverAnswer[randomizer(0,serverAnswer.length)]} 0 "-" "curl/7.47.0"\n`;
+// };
 // let arrMass = [];
+// Геренатор массива строк (искусственное разбиение на части (Не уверен что такое вообще нужно использовать, думал сократит операции использования жесткого диска))
 // function pushArray () {
 // 	arrMass = [];
 // 	while (arrMass.length <= 1000) {
@@ -23,28 +27,39 @@ const stringGenerator = () => {
 // 	}
 // 	return arrMass;
 // }
+//	Проверка на существование файла лога.
+// exists(path,(e)=>{
+// 	const writeStream = fs.createWriteStream(path,{flags:'a+',encoding:'utf8'});
+// 		while (fs.statSync(path).size < 104857600) {
+// 			console.clear();
+// 			// console.log(Math.round(fs.statSync(path).size));
+// 			// pushArray();
+// 			// writeStream.write(stringGenerator()+'');
+// 			// Без стрима
+// 			// fs.writeFileSync(path,stringGenerator(),{flag: 'a', encoding:'utf-8'})
+// 			// Решил сделать синхронно, чтобы виден был процесс
+// 			// fs.writeFile(path,stringGenerator(),{flag: 'a', encoding:'utf-8'}, (err)=>console.log(err))
+// 		}
+// 	writeStream.end(() => console.log('File writing finished'));
+// })
+
+// Задание 2
+const ip = '240.85.195.60';
 const readStream = fs.createReadStream(path, 'utf8');
+const writeStream = fs.createWriteStream(`./storage/filtered_${ip}.log`,{flags:'a+',encoding:'utf8'});
 const tStream = new Transform({
 	transform(chunk, encoding, callback) {
-		const regExp = /d+\.d+\.d+\.d+/g;
+		const regExp = new RegExp(ip, 'g');
+		if (regExp.test(chunk.toString())) {
+			const arr = `${chunk.toString().split('\n').filter((el)=>{
+				return regExp.test(el);
+			}).join('')} \n`
+			this.push(arr)
+		}
+		callback();
 	}
 })
-
-exists(path,(e)=>{
-	const writeStream = fs.createWriteStream(path,{flags:'a+',encoding:'utf8'});
-		while (fs.statSync(path).size < 104857600) {
-			console.clear();
-			// console.log(Math.round(fs.statSync(path).size));
-			// pushArray();
-			// writeStream.write(stringGenerator()+'');
-			// Без стрима
-			fs.writeFileSync(path,stringGenerator(),{flag: 'a', encoding:'utf-8'})
-			// Решил сделать синхронно, чтобы виден был процесс
-			fs.writeFile(path,stringGenerator(),{flag: 'a', encoding:'utf-8'}, (err)=>console.log(err))
-		}
-	writeStream.end(() => console.log('File writing finished'));
-})
-
+readStream.pipe(tStream).pipe(writeStream)
 readStream.on('end',()=>console.log('File reading finished!'));
 
 // readStream.on('data',(chunk)=>{
