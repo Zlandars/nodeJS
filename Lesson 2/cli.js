c17f5d0c309f423c9be624d46fe35aa7569c6b36
Zlandars <mag20102009@gmail.com>
2022-12-05 23:55:19 +0300

    Lesson5 and Lesson4

--- Lesson 2/cli.js
+++ Lesson 2/cli.js
@@ -1,63 +1,48 @@
 #! C:\\Program Files\\nvm\\v18.10.0
 
 import fs from 'fs';
-// import * as readline from "readline";
+import * as readline from "readline";
 import * as path from "path";
 import inquirer from "inquirer";
-// в ес6 перестал работать дирнэйм - костыль из интернетов
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
+import * as color from 'colors';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// yargs на модулях
 import _yargs from 'yargs';
 import {hideBin} from 'yargs/helpers';
-import {Transform} from "stream";
+import colors from "colors";
 
 const yargs = _yargs(hideBin(process.argv));
 
 
 const options = yargs.usage('Usage -p ')
 	.options({
+		'p': {
+			alias: 'path',
+			describe: 'Filter to file',
+			type: 'string',
+			demandOption: false,
+		},
 		'f': {
 			alias: 'filter',
 			describe: 'Filter to file',
 			type: 'string',
-			demandOption: true,
+			demandOption: false,
 		}
 	}).argv;
-// Чтение файла.
-// const rl = readline.createInterface({
-// 	input: process.stdin,
-// 	output: process.stdout
-// });
-// const question = async (query) =>
-// 	new Promise(resolve => rl.question(query,resolve));
-// (async ()=>{
-// 	const filePath = await question('Введите путь к файлу: ');
-// 	const encode = await question('Кодировка: ');
-// 	fs.readFile(path.join(__dirname,filePath),encode,(err,data)=>{
-// 		if (err) return console.log(err);
-// 		console.log(data.toString())
-// 	})
-// 	rl.close()
-// })();
 
-//самовызывающиеся функция
-// Собачьи бубенчики (function () {})()
-// function () {}()
-//	Правильный вид : (function () {}())
-
-let currentDir = __dirname;
+let currentDir = options.p ? path.join(__dirname, options.p) : __dirname;
 
 const checkDir = (filePath) => {
 	return fs.lstatSync(path.join(currentDir, filePath)).isDirectory();
 }
+let counter = 0;
 
 const inq = async () => {
 	// Асинхронный ридер не запускался вообще никак, не понимаю почему
-	const list = await fs.readdirSync(currentDir);
+	const list = fs.readdirSync(currentDir);
 	list.unshift('./')
 	const iteration = await inquirer.prompt([
 		{
@@ -79,25 +64,22 @@ const inq = async () => {
 		return await inq();
 	} else {
 		const readStream = fs.createReadStream(path.join(currentDir, iteration.file), 'utf8');
-		const writeStream = fs.createWriteStream(path.join(__dirname, `./storage/filtered_${options.f}.log`), {
-			flags: 'a+',
-			encoding: 'utf8'
-		});
-		const tStream = await new Transform({
-			transform(chunk, encoding, callback) {
-				console.log(chunk.toString())
-				const regExp = new RegExp(options.f, 'g');
-				if (regExp.test(chunk.toString())) {
-					const arr = `${chunk.toString().split('\n').filter((el) => {
-						return regExp.test(el);
-					}).join('')} \n`
-					this.push(arr)
-				}
-				callback();
+		const rl = readline.createInterface({
+			input: readStream,
+		})
+		rl.on('line', (data)=> {
+			counter++;
+			const regExp = new RegExp(options.f, 'g');
+			console.log(colors.grey(data))
+			if (regExp.test(data.toString())) {
+				const string = data.replace(options.f, (findString) => {
+					return colors.red(findString);
+				})
+				console.log(`Строка номер ${colors.green(counter)}. Найдено совпадение: ${string}`)
+				rl.close();
+				readStream.close();
 			}
 		})
-		readStream.pipe(tStream).pipe(writeStream)
-		readStream.on('end', () => inq());
 	}
 }
 inq();
--- Lesson 3/server.js
+++ Lesson 3/server.js
@@ -4,35 +4,56 @@ import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import fs from "fs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 //Middleware
-app.use('/storage', express.static('storage'))
-app.use((req, res, next) =>{
-	const stringGenerator = () => {
-		const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-		const date = new Date().toISOString();
-		const query = req.method;
-		const serverAnswer = res.statusCode;
-		return `${ip} - - [${date} -0300] "${query} /boo HTTP/1.1" ${serverAnswer} 0 "-" "curl/7.47.0"\n`;
-	};
-	fs.exists(path, () => {
-		fs.writeFile('./storage/access2.log', stringGenerator(), {flag: 'a+', encoding: 'utf-8'}, (err) => err)
-	})
-	next()
-})
+// app.use('/storage', express.static('storage'))
+// app.use((req, res, next) =>{
+// 	const stringGenerator = () => {
+// 		const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+// 		const date = new Date().toISOString();
+// 		const query = req.method;
+// 		const serverAnswer = res.statusCode;
+// 		return `${ip} - - [${date} -0300] "${query} /boo HTTP/1.1" ${serverAnswer} 0 "-" "curl/7.47.0"\n`;
+// 	};
+// 	fs.exists(path, () => {
+// 		fs.writeFile('./storage/access2.log', stringGenerator(), {flag: 'a+', encoding: 'utf-8'}, (err) => err)
+// 	})
+// 	next()
+// })
 // EndMiddleware
-let currentDir = __dirname;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import _yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+
+const yargs = _yargs(hideBin(process.argv));
+
+
+const options = yargs.usage('Usage -p ')
+	.options({
+		'p': {
+			alias: 'path',
+			describe: 'Filter to file',
+			type: 'string',
+			demandOption: false,
+		}
+	}).argv;
+
+let currentDir = options.p ? path.join(__dirname, options.p) : __dirname;
+
+// .replace('%20',' ') КОСТЫЛЬ ДЛЯ ВИНДЫ. На маке не должно быть такого
+// Пробел в адресной строке ломал программу.
 
 const checkDir = (filePath) => {
-	return fs.lstatSync(filePath).isDirectory();
+	return fs.lstatSync(filePath.replace('%20',' ')).isDirectory();
 }
 
 
 app.get('/', function (req, res) {
-	(async () => {
-		const list = await fs.readdirSync(currentDir);
+	(() => {
+		const list = fs.readdirSync(currentDir);
 		res.send(`<body>
 			<div style="display: flex; flex-direction:column;">
 			${list.map((item,index)=>{
@@ -47,7 +68,7 @@ app.get('*', function (req, res) {
 	if(req.url === '/favicon.ico') return;
 	const newPath = path.join(currentDir, req.url.split('/').filter(function (el) {
 		return (el != null && el != "" || el === 0);
-	}).join('/'));
+	}).join('/')).replace('%20',' ');
 	(async () => {
 		if(!checkDir(newPath)) {
 			const readStream = await fs.createReadStream(newPath, 'utf8');
@@ -66,7 +87,7 @@ app.get('*', function (req, res) {
 			});
 			readStream.on('end',()=>console.log('Read stream end'));
 		} else {
-			const list = await fs.readdirSync(newPath);
+			const list = fs.readdirSync(newPath);
 			let arrPath = req.url.split('/').filter(function (el) {
 				return (el != null && el != "" || el === 0);
 			});
@@ -74,7 +95,6 @@ app.get('*', function (req, res) {
 				<div style="display: flex; flex-direction:column;">
 				<a href='${arrPath.length > 1 ? '/' : ''}${arrPath.slice(0,arrPath.length - 1).join('/')}/'><button>Back</button></a>
 					${list.map((item,index)=>{
-						console.log(item)
 						return `<a href='/${req.url.split('/').filter(function (el) {
 							return (el != null && el != "" || el === 0);
 						}).join('/')}/${item}'><button id="${index}">${item}</button></a>`
